28a1654351879955c3cff5f70af11db509dab3db
kishor <kishorebsc12@gmail.com>
2025-09-30 18:54:53 +0530

    added

--- argocd.sh
+++ argocd.sh
@@ -1,5 +1,5 @@
-URL= $(kubectl get svc argocd-server -n argocd | grep argocd | awk '{print $4}')
-PASSWORD= $(kubectl get secrets -n argocd argocd-initial-admin-secret -o json | jq .data.password | xargs | base64 --decode)
+URL=$(kubectl get svc argocd-server -n argocd | grep argocd | awk '{print $4}')
+PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json | jq .data.password | xargs | base64 --decode)
 argocd login $URL --grpc-web --insecure --username admin --password $PASSWORD
 
 component_name=$1
