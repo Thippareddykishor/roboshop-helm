46ef96ce25631062b372213f295cfa851624a78a
kishor <kishorebsc12@gmail.com>
2025-10-11 15:00:30 +0530

    added

--- argocd.sh
+++ argocd.sh
@@ -5,7 +5,8 @@ env=$2
 imageTag=$3
 
 PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json | jq .data.password | xargs | base64 --decode)
-argocd login argocd-${env}.kommanuthala.store --grpc-web --insecure --username admin --password $PASSWORD
+argocd login afc5ae808770a416180e4d064301a0e0-601377892.us-east-1.elb.amazonaws.com --grpc-web --insecure --username admin --password $PASSWORD
+
 
 argocd app create ${component_name} --repo https://github.com/thippareddykishor/roboshop-helm --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values env-${env}/${component_name}.yaml --helm-set imageTag=${imageTag}
 argocd app sync ${component_name}
