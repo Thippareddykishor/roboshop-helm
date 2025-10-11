6cecb190d340a4a563eb6e4dd5e48603a4eb6f57
kishor <kishorebsc12@gmail.com>
2025-10-11 14:54:24 +0530

    added

--- argocd.sh
+++ argocd.sh
@@ -1,23 +1,23 @@
+
 # URL=$(kubectl get svc argocd-server -n argocd | grep argocd | awk '{print $4}')
-# PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json | jq .data.password | xargs | base64 --decode)
-# argocd login $URL --grpc-web --insecure --username admin --password $PASSWORD
+component_name=$1
+env=$2
+imageTag=$3
+
+PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json | jq .data.password | xargs | base64 --decode)
+argocd login argocd-${env}.kommanuthala.store --grpc-web --insecure --username admin --password $PASSWORD
+
+argocd app create ${component_name} --repo https://github.com/thippareddykishor/roboshop-helm --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values env-${env}/${component_name}.yaml --helm-set imageTag=${imageTag}
+argocd app sync ${component_name}
 
 # component_name=$1
 # env=$2
 # imageTag=$3
 
+# kubectl create ns app || true
 
-# argocd app create ${component_name} --repo https://github.com/thippareddykishor/roboshop-helm --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values env-${env}/${component_name}.yaml --helm-set imageTag=${imageTag}
+# PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json | jq .data.password|xargs | base64 --decode)
+# argocd login argocd-${env}.kommanuthala.store --grpc-web --insecure --username admin --password $PASSWORD
+
+# argocd app create ${component_name} --upsert --repo https://github.com/thippareddykishor/roboshop-helm --path . --dest-namespace app --dest-server https://kubernetes.default.svc --values env-${env}/${component_name}.yaml --helm-set imageTag=${imageTag}
 # argocd app sync ${component_name}
-
-component_name=$1
-env=$2
-imageTag=$3
-
-kubectl create ns app || true
-
-PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json | jq .data.password|xargs | base64 --decode)
-argocd login argocd-${env}.kommanuthala.store --grpc-web --insecure --username admin --password $PASSWORD
-
-argocd app create ${component_name} --upsert --repo https://github.com/thippareddykishor/roboshop-helm --path . --dest-namespace app --dest-server https://kubernetes.default.svc --values env-${env}/${component_name}.yaml --helm-set imageTag=${imageTag}
-argocd app sync ${component_name}
